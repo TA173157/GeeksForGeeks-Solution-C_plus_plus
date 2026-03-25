312021df1de949eb6e461cf08c0ad35d231902da
TA173157 <tamusic2147k@gmail.com>
2026-03-25 20:03:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Mirror Tree/mirror-tree.cpp
@@ -0,0 +1,25 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void mirror(Node* root) {
+        if(!root)return;
+        Node* temp = root->right;
+        root->right = root->left;
+        root->left = temp;
+        mirror(root->left);
+        mirror(root->right);
+    }
+};
